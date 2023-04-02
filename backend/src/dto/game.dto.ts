707ea0474521c6aefa044880b902587953acd1c2
Jiwon Park <jpark2@c2r6s8.42seoul.kr>
2023-04-02 14:44:16 +0900

    modify lobby dto and fix error

--- backend/src/dto/game.dto.ts
+++ backend/src/dto/game.dto.ts
@@ -1,9 +1,8 @@
 export class GameDto {
-	title: string;
-	interupt_mode: boolean;
-	private_mode: boolean;
-	password: string;
-	playing: boolean;
-	players: string[];
-	users: string[];
+  title: string;
+  interupt_mode: boolean;
+  private_mode: boolean;
+  password: string;
+  playing: boolean;
+  count: number;
 }
--- backend/src/game/game.controller.ts
+++ backend/src/game/game.controller.ts
@@ -7,48 +7,53 @@ import { AuthService } from 'src/auth/auth.service';
 
 @Controller('/game')
 export class GameController {
-	constructor(
-		private authService: AuthService,
-		private gameService: GameService,
-	) {}
+  constructor(
+    private authService: AuthService,
+    private gameService: GameService,
+  ) {}
 
-	@Get('/lobby')
-	// @UseGuards(JwtGuard)
-	async lobby() {
-		let games = await this.gameService.getLobbyInfo();
-		if (games.length === 0) { // test code => TODO: delete
-			const user1 = {
-				user_id: 123,
-				intra_id: 'asdf'
-			};
-			const user2 = {
-				user_id: 124,
-				intra_id: 'qwer'
-			}
-			await this.authService.addUser(user1);
-			await this.authService.addUser(user2);
-			let found_user = await this.authService.findUserByIntraId(user1.intra_id);
-			this.gameService.createGame({
-				title: 'game1',
-				interupt_mode: false,
-				private_mode: true,
-				password: 'asdf',
-				playing: false,
-				players: [],
-				users: [],
-			}, found_user);
-			found_user = await this.authService.findUserByIntraId(user2.intra_id);
-			this.gameService.createGame({
-				title: 'game2',
-				interupt_mode: true,
-				private_mode: false,
-				password: '',
-				playing: false,
-				players: [],
-				users: [],
-			}, found_user);
-			games = await this.gameService.getLobbyInfo();
-		}
-		return games;
-	}
+  @Get('/lobby')
+  @UseGuards(JwtGuard)
+  async lobby() {
+    let games = await this.gameService.getLobbyInfo();
+    if (games.length === 0) {
+      // test code => TODO: delete
+      const user1 = {
+        user_id: 123,
+        intra_id: 'asdf',
+      };
+      const user2 = {
+        user_id: 124,
+        intra_id: 'qwer',
+      };
+      await this.authService.addUser(user1);
+      await this.authService.addUser(user2);
+      let found_user = await this.authService.findUserByIntraId(user1.intra_id);
+      this.gameService.createGame(
+        {
+          title: 'game1',
+          interupt_mode: false,
+          private_mode: true,
+          password: 'asdf',
+          playing: false,
+          count: 1,
+        },
+        found_user,
+      );
+      found_user = await this.authService.findUserByIntraId(user2.intra_id);
+      this.gameService.createGame(
+        {
+          title: 'game2',
+          interupt_mode: true,
+          private_mode: false,
+          password: '',
+          playing: false,
+          count: 1,
+        },
+        found_user,
+      );
+      games = await this.gameService.getLobbyInfo();
+    }
+    return games;
+  }
 }
