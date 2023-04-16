2f89bec4646e20fd3d1c7bae9a318c5fb77c1557
oyhoyhk <oyhoyhk@naver.com>
2023-04-16 13:56:15 +0900

    [FE] nth-child -> nth-of-type으로 변경, history 시간 내림차순으로 정렬

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -34,62 +34,62 @@ const LoadingContainer = styled.div`
     border-radius: 50%;
     animation: lds-default 1.2s linear infinite;
   }
-  &.lds-default div:nth-child(1) {
+  &.lds-default div:nth-of-type(1) {
     animation-delay: 0s;
     top: 37px;
     left: 66px;
   }
-  &.lds-default div:nth-child(2) {
+  &.lds-default div:nth-of-type(2) {
     animation-delay: -0.1s;
     top: 22px;
     left: 62px;
   }
-  &.lds-default div:nth-child(3) {
+  &.lds-default div:nth-of-type(3) {
     animation-delay: -0.2s;
     top: 11px;
     left: 52px;
   }
-  &.lds-default div:nth-child(4) {
+  &.lds-default div:nth-of-type(4) {
     animation-delay: -0.3s;
     top: 7px;
     left: 37px;
   }
-  &.lds-default div:nth-child(5) {
+  &.lds-default div:nth-of-type(5) {
     animation-delay: -0.4s;
     top: 11px;
     left: 22px;
   }
-  &.lds-default div:nth-child(6) {
+  &.lds-default div:nth-of-type(6) {
     animation-delay: -0.5s;
     top: 22px;
     left: 11px;
   }
-  &.lds-default div:nth-child(7) {
+  &.lds-default div:nth-of-type(7) {
     animation-delay: -0.6s;
     top: 37px;
     left: 7px;
   }
-  &.lds-default div:nth-child(8) {
+  &.lds-default div:nth-of-type(8) {
     animation-delay: -0.7s;
     top: 52px;
     left: 11px;
   }
-  &.lds-default div:nth-child(9) {
+  &.lds-default div:nth-of-type(9) {
     animation-delay: -0.8s;
     top: 62px;
     left: 22px;
   }
-  &.lds-default div:nth-child(10) {
+  &.lds-default div:nth-of-type(10) {
     animation-delay: -0.9s;
     top: 66px;
     left: 37px;
   }
-  &.lds-default div:nth-child(11) {
+  &.lds-default div:nth-of-type(11) {
     animation-delay: -1s;
     top: 62px;
     left: 52px;
   }
-  &.lds-default div:nth-child(12) {
+  &.lds-default div:nth-of-type(12) {
     animation-delay: -1.1s;
     top: 52px;
     left: 62px;
