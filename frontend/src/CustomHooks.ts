1e373acb7664c69c733786d727b51edb30d7f9bb
oyhoyhk <oyhoyhk@naver.com>
2023-04-24 19:09:15 +0900

    [FE] ㅊㅐ팅 로그 꺼내올 때 자료형 변경

--- frontend/src/CustomHooks.ts
+++ frontend/src/CustomHooks.ts
@@ -1,7 +1,8 @@
 export const convertTimeForChat = (date: Date) => {
-  const h = date.getHours() % 12,
-    m = date.getMinutes();
-  return `${date.getHours() < 12 ? "AM" : "PM"} ${
+  const time = new Date(date);
+  const h = time.getHours() % 12,
+    m = time.getMinutes();
+  return `${time.getHours() < 12 ? "AM" : "PM"} ${
     h === 0 ? 12 : h < 10 ? "0" + h : h
   }:${m < 10 ? "0" + m : m}`;
 };
