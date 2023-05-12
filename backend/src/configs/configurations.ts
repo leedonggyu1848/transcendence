601ea1a947eecbb49db98ce51f9a7e1e4d389f87
Jiwon Park <jpark2@jpark2.42seoul.kr>
2023-05-12 15:51:41 +0900

    [BE] 몰라 안해

--- backend/src/configs/configurations.ts
+++ backend/src/configs/configurations.ts
@@ -1,7 +1,7 @@
 export default () => ({
-	port: 3000,
-	database: {
-		host: process.env.DATABASE_HOST,
-		port: 5432
-	}
+  port: 3000,
+  database: {
+    host: 'postgres',
+    port: 5432,
+  },
 });
